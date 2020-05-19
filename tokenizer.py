89cad69e95412dac3c5cf403cc199cbca53085fe
Francis Macapobre <f.macapobre@gmail.com>
2020-05-19 15:12:10 -0700

    functioning RESERVEWORD replacement

--- tokenizer.py
+++ tokenizer.py
@@ -1,13 +1,30 @@
+import re
+
 test = open("test-input.mz", "r")
 program = test.read()
 print(program)
 
+literals = ["Title:", "Time:", "Tempo", "Composer", "Arranged by", "Key", "seq", "main", "= [", "|T|", "|B|", "||", "]", ","]
+
 class Tokenizer:
+
   def __init__(self, program):
     self.program = program
 
   def printProgram(self):
     print("======Program======\n" + self.program)
 
+  def tokenize(self):
+    tokenizedProgram = self.program + "TOKENIZED"
+    tokenizedProgram = tokenizedProgram.replace('\n', '')
+    print(tokenizedProgram)
+
+    for l in literals:
+      tokenizedProgram = tokenizedProgram.replace(l, "_" + l + "_")
+    
+    print("REPLACED RW====================\n" + tokenizedProgram)
+
+
 t = Tokenizer(program)
 t.printProgram()
+t.tokenize()
