a0901f7702d694507d08954b797a7c063a92fe8c
Francis Macapobre <f.macapobre@gmail.com>
2020-05-19 15:24:24 -0700

    tokenized

--- tokenizer.py
+++ tokenizer.py
@@ -4,7 +4,7 @@ test = open("test-input.mz", "r")
 program = test.read()
 print(program)
 
-literals = ["Title:", "Time:", "Tempo", "Composer", "Arranged by", "Key", "seq", "main", "= [", "|T|", "|B|", "||", "]", ","]
+literals = ["Title:", "Time:", "Tempo:", "Composer:", "Arranged by:", "Key:", "seq", "main", "= [", "|T|", "|B|", "||", "]", ","]
 
 class Tokenizer:
 
@@ -15,15 +15,24 @@ class Tokenizer:
     print("======Program======\n" + self.program)
 
   def tokenize(self):
-    tokenizedProgram = self.program + "TOKENIZED"
+    tokenizedProgram = self.program
     tokenizedProgram = tokenizedProgram.replace('\n', '')
     print(tokenizedProgram)
 
     for l in literals:
       tokenizedProgram = tokenizedProgram.replace(l, "_" + l + "_")
     
-    print("REPLACED RW====================\n" + tokenizedProgram)
 
+    tokenizedProgram = tokenizedProgram.replace("__", "_")
+
+    if (len(tokenizedProgram) > 0 and tokenizedProgram[0] == '_'):
+      tokenizedProgram = tokenizedProgram[1:]
+
+    tokens = tokenizedProgram.split('_')
+    for t in tokens:
+      print(t)
+
+  
 
 t = Tokenizer(program)
 t.printProgram()
