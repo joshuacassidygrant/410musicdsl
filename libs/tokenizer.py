f4fe813d53f39a004a8ad5afe4bbae1a9ff0df2b
Francis Macapobre <f.macapobre@gmail.com>
2020-05-19 21:01:52 -0700

    impl singleton tokenizer

--- libs/tokenizer.py
+++ libs/tokenizer.py
@@ -22,21 +22,21 @@ class Tokenizer:
       theTokenizer = Tokenizer(program, literals)
 
   def getTokenizer(self):
-    return Tokenizer
+    return Tokenizer.theTokenizer
 
   def tokenize(self):
     tokenizedProgram = Tokenizer.program
-    print("1: " + tokenizedProgram)
+    print("1: " + tokenizedProgram + "\n")
 
     tokenizedProgram = tokenizedProgram.replace('\n', '')
     tokenizedProgram = tokenizedProgram.strip()
-    print("2: " + tokenizedProgram)
+    print("2: " + tokenizedProgram + "\n")
 
     for l in Tokenizer.literals:
       tokenizedProgram = tokenizedProgram.replace(l, "_" + l + "_")
     
     tokenizedProgram = tokenizedProgram.replace("__", "_")
-    print("3: " + tokenizedProgram)
+    print("3: " + tokenizedProgram + "\n")
 
     if (len(tokenizedProgram) > 0 and tokenizedProgram[0] == '_'):
       tokenizedProgram = tokenizedProgram[1:]
@@ -44,12 +44,42 @@ class Tokenizer:
     tokens = tokenizedProgram.split('_')
     tokens = [t.strip() for t in tokens]
     tokens = [t for t in tokens if t != '']
-    print("4: ", tokens)
+    print("4: ", tokens, "\n")
     Tokenizer.tokens = tokens
-    print("5: ", Tokenizer.tokens)
+    print("5: ", Tokenizer.tokens, "\n")
+
+  def checkNext(self):
+    token = ""
+    if (Tokenizer.currentToken < len(Tokenizer.tokens)):
+      token = Tokenizer.tokens[Tokenizer.currentToken]
+    else:
+      token = "NO_MORE_TOKENS"
+    return token
+
+  def getNext(self):
+    token = ""
+    if (Tokenizer.currentToken < len(Tokenizer.tokens)):
+      token = Tokenizer.tokens[Tokenizer.currentToken]
+      Tokenizer.currentToken += 1
+    else:
+      token = "NULLTOKEN"
+
+  def checkToken(self, regexp):
+    s = self.checkNext()
+    print("comparing: |"+s+"|  to  |"+regexp+"|")
+    return bool(re.match(regexp, s))
+
+  def getAndCheckNext(self, regexp):
+    s = self.getNext()
+    if not bool(re.match(regexp, s)):
+      raise Exception("Unexpected next token for Parsing! Expected something matching: " + regexp + " but got: " + s) 
+    print("matched: "+s+"  to  "+regexp)
+    return s
+  
+  def moreTokens(self):
+    return Tokenizer.currentToken < len(Tokenizer.tokens)
 
   @staticmethod
   def printProgram():
-    print("WAS CALLED")
-    print("=====Program=====/n" + Tokenizer.program)
+    print("=====Program=====\n" + Tokenizer.program)
 
