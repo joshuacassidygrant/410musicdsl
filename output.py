6a00d327f940ed7d959c02961704cc94be6af5b6
Title-Jirakul <titlejirakul@gmail.com>
2020-05-24 21:18:09 -0700

    implemented to pdf

--- output.py
+++ output.py
@@ -1,37 +1,57 @@
 import mingus.core.notes as notes
-import mingus.core.keys as Key
-from mingus.containers import Bar, NoteContainer
-# import lilypond as LilyPond
+from mingus.containers import Bar, NoteContainer, Track, Composition
 import mingus.extra.lilypond as LilyPond
 from mingus.midi import midi_file_out
+from input import Input
+
+# NOTE: must install lilypond.exe (for Windiws) and pip install mingus before running
+# LilyPond in the $PATH is needed.
 
 # Create midi file
-# @parameters list of note string
 def createMidi(noteList):
     if not noteList:
         print("Invalid: List of notes is empty")
     else:
         # nc = NoteContainer(noteList)
-        b = Bar()
+        # midi_file_out.write_NoteContainer("music1.mid", nc)
         for note in noteList:
             b + note
-        # midi_file_out.write_NoteContainer("music1.mid", nc)
         midi_file_out.write_Bar("music.mid", b, 120, 1)
 
 # Create music sheet PDF
-# @parameters list of note string
-def createMusicsheet(noteList):
-    if not noteList:
-        print("Invalid: List of notes is empty")
+def createMusicsheet(input):
+    if not input:
+        print("Invalid: input is empty")
     else:
-        b = Bar()
-        for note in noteList:
-            b + note
-        bar = LilyPond.from_Bar(b)
-        print(bar)
-        LilyPond.to_pdf(bar, "my_first_bar")
+        c = Composition()
+        subtitle = "time = " + input.time + ", tempo = " + input.tempo + ", composer = " + input.composer + ", year = " + input.year + ", key = " + input.key
+        c.set_author(input.arrangedBy, 'author@email.com')
+        c.set_title(input.title, subtitle)
+        t = createTrack(input.body)
+        c.add_track(t)
+        compString = LilyPond.from_Composition(c)
+        LilyPond.to_pdf(compString, input.title)
 
-# Example call
-# noteList = ["C", "E", "G", "B","C", "E", "G", "B"]
-# createMusicsheet(noteList)
-# createMidi(noteList)
+def createTrack(body):
+    t = Track()
+    key = body["key"]
+    meter = body['meter']
+    bars = body['bars']
+    # Create individual bar
+    for bar in bars:
+        b = Bar(key)
+        if meter:
+            b.set_meter(meter)
+        for item in bar:
+            #TODO: major and minor
+            (note, duration) = item
+            b.place_notes(note, duration)
+        t += b
+    return t
+
+def main():
+    example = Input()
+    createMusicsheet(example)
+
+if __name__ == "__main__":
+    main()
