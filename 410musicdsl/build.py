dfc4f7a03e2419d75357d1912b72a17e851a2b28
Francis Macapobre <f.macapobre@gmail.com>
2020-05-27 17:32:41 -0700

    method stubs in build

--- 410musicdsl/build.py
+++ 410musicdsl/build.py
@@ -2,3 +2,21 @@ class Input(object):
   
   def __init__(self):
     pass
+
+  def setTitle(self, title): pass
+
+  def setTime(self, time): pass
+
+  def setTempo(self, temp): pass
+
+  def setComposer(self, composer): pass
+
+  def setYear(self, year): pass
+
+  def setArrangedBy(self, arrangeBy): pass
+
+  def setKey(self, key): pass
+
+  def updateBody(self, key, val): pass
+
+  def updateBodyBars(self, val): pass
